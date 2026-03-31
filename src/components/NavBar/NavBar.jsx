738a2cae0d8f8bddbc6e7383775daea5c7199e63
Edu <luis_100404@hotmail.com>
2026-03-31 10:57:32 -0300

    add files

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -40,8 +40,8 @@ export default function NavBar() {
             <Link to="/dashboard" onClick={closeMenu}>Home</Link>
             <Link to="/caixaDiario" onClick={closeMenu}>Caixa</Link>
             <Link to="/reverse_pricing" onClick={closeMenu}>Preço Reverso</Link>
-            <Link to="/calculadora" onClick={closeMenu}>Calculadora Maquininha</Link>
-            <Link to="/taxas" onClick={closeMenu}>Minhas Taxas</Link>
+            <Link to="/calculadora" onClick={closeMenu}>Maquininhas</Link>
+            <Link to="/taxas" onClick={closeMenu}>Taxas</Link>
             <Link to="/sobre" onClick={closeMenu}>Sobre</Link>
             <Link to="/contato" onClick={closeMenu}>Contato</Link>
             <FiLogOut 
