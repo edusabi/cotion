31e4e9d1772a05e715d30480f610c9fabb5360c9
Edu <luis_100404@hotmail.com>
2026-03-31 08:58:17 -0300

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // 1. ADICIONE O IMPORT AQUI (junto com os outros das páginas)
 import Success from "./pages/Success/Success"; 
 
+///components
 import NavBar from "./components/NavBar/NavBar";
 import NotFound from "./components/Notfound/Notfound";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+
+///pages
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
@@ -13,8 +17,7 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Dashboard from './pages/Dashboard/Dashboard';
 import CaixaDiario from './pages/CaixaDiario/CaixaDiario';
-import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
-import ReversePricing from "./components/ReversePricing/ReversePricing";
+import ReversePricing from "./pages/ReversePricing/ReversePricing";
 
 function App() {
   return (
