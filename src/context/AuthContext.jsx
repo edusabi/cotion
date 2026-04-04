9ec9ad7e5b5114c1bd2c9253bafd56e1b4f7e85c
Edu <luis_100404@hotmail.com>
2026-04-04 08:43:30 -0300

    update files

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -9,7 +9,7 @@ export function AuthProvider({ children }) {
 
   // 🔥 verifica se já está logado ao iniciar
   useEffect(() => {
-    axios.get("https://cotion.discloud.app/me", {
+    axios.get("/api/me", {
       withCredentials: true
     })
     .then(res => setUser(res.data))
@@ -20,7 +20,7 @@ export function AuthProvider({ children }) {
   // 🔐 login
   async function login(email, password) {
     const res = await axios.post(
-      "https://cotion.discloud.app/auth/login",
+      "/api/auth/login",
       { email, password },
       { withCredentials: true }
     );
